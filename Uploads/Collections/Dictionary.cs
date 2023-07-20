85a756c628cd4b4e0597d942b81f73c4ff6a5096
Karthikdecoder <139344592+Karthikdecoder@users.noreply.github.com>
2023-07-20 13:22:54 +0530

    Add files via upload

---
+++ Uploads/Collections/Dictionary.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections.Generic;
+using System.Xml;
+
+public class Program
+{
+    public static void Main()
+    {
+        Dictionary<string, object> d = new Dictionary<string, object>();
+
+        for(int i = 0; i < 5; i++)
+        {
+            string key = System.Console.ReadLine();
+            object obj = int.Parse(System.Console.ReadLine());
+            d.Add(key, obj);
+        }
+
+        foreach(string key in d.Keys)
+        {
+            System.Console.WriteLine(key + ":" + d[key]);
+        }
+
+        System.Console.ReadKey();
+    }
+}
