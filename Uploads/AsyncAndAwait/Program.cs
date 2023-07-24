0d3bd2971dcc5e55c08644d69a2b435a107cc256
Karthikdecoder <139344592+Karthikdecoder@users.noreply.github.com>
2023-07-24 11:24:46 +0530

    Add files via upload

---
+++ Uploads/AsyncAndAwait/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        Method1();
+        Method2();
+
+        Console.ReadKey();
+    }
+
+    public static async Task Method1()
+    {
+        await Task.Run(() =>
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Method 1 is running");
+                Task.Delay(1000).Wait();
+            }
+        });
+    }
+
+    public static void Method2()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Method 2 is running");
+            Task.Delay(1000).Wait();
+        }
+    }
+}
+
+
